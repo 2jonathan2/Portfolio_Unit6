7385e50dc27365e40f5665bd3eeb9ac31381505b
Jonathan Aca <jonathanaca22@gmail.com>
2016-12-19 22:49:50 +0000

    I did stuff today

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
-      $("#like").html("You like this project!");
+      $("#like").html("Hello!");
     });
+    $("button").click(function() {
+        $("#like").css("color","green");
+    });
+
     
     
     
-    
-});
